b4d1d2eeba9c6fe6be34d21be38e4450d935bdd7
Guillaume Benini <guillaume_benini@hotmail.com>
2013-01-28 23:15:25 -0500

    New entity system.

--- src/org/thematics/World.java
+++ src/org/thematics/World.java
@@ -1,5 +1,10 @@
 package org.thematics;
 
+/**
+ * The world containing entities and objects.
+ * @author Guillaume
+ *
+ */
 public class World {
 
 	
---
+++ src/org/thematics/player/Inventory.java
@@ -0,0 +1,10 @@
+package org.thematics.player;
+
+/**
+ * The player's inventory.
+ * @author Guillaume
+ *
+ */
+public class Inventory {
+
+}
